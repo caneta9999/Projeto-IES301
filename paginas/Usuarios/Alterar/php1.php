9169fc288fdc29dc9c01a066959fa1ef6b7cf652
caneta9999 <93010461+caneta9999@users.noreply.github.com>
2022-05-08 15:17:49 -0300

    Adm não pode alterar outro adm só ele mesmo

--- paginas/Usuarios/Alterar/php1.php
+++ paginas/Usuarios/Alterar/php1.php
@@ -28,27 +28,34 @@ if($send || $_SESSION['alterarProprioUsuario']){
                 $variavelControle = 0;
 				$_SESSION['mensagemErro'] = "Não há usuário com esse id!";}}
         if($variavelControle){    
-            $result = "SELECT * FROM $db.$TB_USUARIO WHERE idUsuario=:idUsuario";
+            $result = "SELECT Tipo, Administrador FROM $db.$TB_USUARIO WHERE idUsuario=:idUsuario";
             $select = $conx->prepare($result);
             $select->execute(['idUsuario' => $id]);
             $tipo = 0;
             foreach($select->fetchAll() as $linha_array) {
                 $tipo = $linha_array['Tipo'];
+				$administrador = $linha_array['Administrador'];
             }
-            if($tipo == 2){
-                $result = "SELECT U1.idUsuario,".'U1.Login'.",U1.Senha,U1.Nome,U1.Administrador,U1.Cpf,U1.Tipo,A1.Matricula,A1.Curso_idCurso,C1.Nome 'NomeCurso',U1.Ativo FROM $db.$TB_USUARIO U1 inner join $db.$TB_ALUNO A1 On U1.idUsuario = A1.Usuario_idUsuario inner join $db.$TB_CURSO C1 On A1.Curso_idCurso = C1.idCurso WHERE A1.Usuario_idUsuario=:usuario";
-                $select = $conx->prepare($result);
-                $select->execute(['usuario' => $id]);
-                $_SESSION['queryUsuario3'] = $select->fetchAll();
-            }else{
-                $result = "SELECT * FROM $db.$TB_USUARIO WHERE idUsuario=:idUsuario";
-                $select = $conx->prepare($result);
-                $select->execute(['idUsuario' => $id]);
-                $_SESSION['queryUsuario3'] = $select->fetchAll();
-            }
-            $_SESSION['mensagemFinalizacao'] =  'Operação finalizada com sucesso!';}
-        header("Location: ./alterar.php");	
-        }
+			if($administrador && $id != $_SESSION['idUsuarioLogin']){
+				$_SESSION['mensagemErro'] = 'Administrador, que não seja você mesmo, não pode ser alterado!';
+			}
+			else{
+				if($tipo == 2){
+					$result = "SELECT U1.idUsuario,".'U1.Login'.",U1.Senha,U1.Nome,U1.Administrador,U1.Cpf,U1.Tipo,A1.Matricula,A1.Curso_idCurso,C1.Nome 'NomeCurso',U1.Ativo FROM $db.$TB_USUARIO U1 inner join $db.$TB_ALUNO A1 On U1.idUsuario = A1.Usuario_idUsuario inner join $db.$TB_CURSO C1 On A1.Curso_idCurso = C1.idCurso WHERE A1.Usuario_idUsuario=:usuario";
+					$select = $conx->prepare($result);
+					$select->execute(['usuario' => $id]);
+					$_SESSION['queryUsuario3'] = $select->fetchAll();
+				}else{
+					$result = "SELECT * FROM $db.$TB_USUARIO WHERE idUsuario=:idUsuario";
+					$select = $conx->prepare($result);
+					$select->execute(['idUsuario' => $id]);
+					$_SESSION['queryUsuario3'] = $select->fetchAll();
+				}	
+				$_SESSION['mensagemFinalizacao'] =  'Operação finalizada com sucesso!';
+			}
+		}
+		header("Location: ./alterar.php");	
+    }
     catch(PDOException $e) {
             $msgErr = "Erro na consulta:<br />";
             $_SESSION['mensagemErro'] = $msgErr;     
