adafb29f97eeb759c7b7cdf1b95052d673889889
caneta9999 <93010461+caneta9999@users.noreply.github.com>
2022-05-20 09:52:20 -0300

    Pequena mudança

--- paginas/Criticas/Estatisticas/estatisticas.php
+++ paginas/Criticas/Estatisticas/estatisticas.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['idUsuarioLogin']) || (!$_SESSION['administradorLogin'] && $
             break;
         }
     }
-    $result = "SELECT D1.Código, PD1.idProfessorDisciplina, D1.Nome 'DisciplinaNome',U1.Nome 'ProfessorNome', PD1.Periodo, PD1.DiaSemana FROM $db.$TB_PROFESSORDISCIPLINA PD1 inner join $db.$TB_DISCIPLINA D1 ON PD1.Disciplina_idDisciplina = D1.idDisciplina inner join $db.$TB_PROFESSOR P1 On P1.idProfessor = PD1.Professor_idProfessor inner join $db.$TB_USUARIO U1 on P1.Usuario_idUsuario = U1.idUsuario where PD1.Professor_idProfessor like :idProfessor order by D1.Nome";
+    $result = "SELECT D1.Código, PD1.idProfessorDisciplina, D1.Nome 'DisciplinaNome',U1.Nome 'ProfessorNome', PD1.Periodo, PD1.DiaSemana FROM $db.$TB_PROFESSORDISCIPLINA PD1 inner join $db.$TB_DISCIPLINA D1 ON PD1.Disciplina_idDisciplina = D1.idDisciplina inner join $db.$TB_PROFESSOR P1 On P1.idProfessor = PD1.Professor_idProfessor inner join $db.$TB_USUARIO U1 on P1.Usuario_idUsuario = U1.idUsuario where PD1.Professor_idProfessor like :idProfessor order by D1.Código";
     $select = $conx->prepare($result);
     $select->bindParam(':idProfessor',$idProfessor);
     $select->execute();
